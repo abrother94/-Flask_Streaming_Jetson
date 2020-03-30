9a9a2c6d2a2b3a3df8d4e6d5f9a16683c5bdd1b9
abrother94 <abrother94@gmail.com>
2020-03-30 11:01:53 +0800

    update

--- camera_pi.py
+++ camera_pi.py
@@ -1,6 +1,7 @@
 import io
 import time
 import picamera
+import datetime as dt
 from base_camera import BaseCamera
 
 
@@ -12,6 +13,10 @@ class Camera(BaseCamera):
             time.sleep(2)
 
             stream = io.BytesIO()
+            camera.resolution = (1640,1232)
+            camera.rotation = -90
+            camera.annotate_background = picamera.Color('black')
+            camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             for _ in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
                 # return current frame
